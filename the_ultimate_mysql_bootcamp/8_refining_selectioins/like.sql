73826b7c584702f734d8d98bbaec0e95b20a556f
giorgos-grekis <georgrekis.github@gmail.com>
2024-12-11 18:18:43 +0200

    like

---
+++ the_ultimate_mysql_bootcamp/8_refining_selectioins/like.sql
@@ -0,0 +1,27 @@
+-- '%da%' => the '%' is wildcards, means any number of characters in front and in the end of 'da' word
+SELECT
+    title,
+    author_fname,
+    author_lname
+FROM
+    books
+WHERE
+    author_fname LIKE '%da%';
+
+-- title included ':' 
+SELECT
+    title,
+    author_fname,
+    author_lname
+FROM
+    books
+WHERE
+    title LIKE '%:%';
+
+-- return all the first name ends with 'n'
+SELECT
+    *
+FROM
+    books
+WHERE
+    author_fname LIKE '%n';
