f6e490a426e3969fd73391e36daec0e20cf55ab7
giorgos-grekis <georgrekis.github@gmail.com>
2024-12-13 15:37:54 +0200

    Subqueries

--- the_ultimate_mysql_bootcamp/9_aggregate_functions/max_min.sql
+++ the_ultimate_mysql_bootcamp/9_aggregate_functions/max_min.sql
@@ -3,3 +3,20 @@ FROM books;
 -- max pages
 SELECT MIN(pages)
 FROM books;
+--  Subqueries
+-- first run the query inside of parentheses 
+-- and the query outside 
+SELECT *
+FROM books
+WHERE pages = (
+        SELECT Max(pages)
+        FROM books
+    );
+-- select the older book
+SELECT title,
+    released_year
+FROM books
+WHERE released_year = (
+        SELECT MIN(released_year)
+        FROM books
+    );
