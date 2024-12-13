f075424ae5bd8d7ec07048204d5b59a89c91efbc
giorgos-grekis <georgrekis.github@gmail.com>
2024-12-13 18:17:33 +0200

    Grouping By Multiple Columns min_max group by

--- the_ultimate_mysql_bootcamp/9_aggregate_functions/group_by.sql
+++ the_ultimate_mysql_bootcamp/9_aggregate_functions/group_by.sql
@@ -13,3 +13,9 @@ SELECT author_lname,
 FROM books
 GROUP BY author_lname
 ORDER BY books_written DESC;
+-- Grouping By Multiple Columns (GROUP BY author_lname, author_fname)
+SELECT CONCAT(author_lname, ' ', author_fname) as author,
+    COUNT(*)
+FROM books
+GROUP BY author_lname,
+    author_fname;
--- the_ultimate_mysql_bootcamp/9_aggregate_functions/max_min.sql
+++ the_ultimate_mysql_bootcamp/9_aggregate_functions/max_min.sql
@@ -20,3 +20,14 @@ WHERE released_year = (
         SELECT MIN(released_year)
         FROM books
     );
+-- min max with group by
+-- find the year each author published 
+SELECT author_fname,
+    author_lname,
+    COUNT(*) AS books_written,
+    MIN(released_year) AS earliest_release,
+    MAX(released_year) AS latest_release,
+    MAX(pages) AS longest_page_count
+FROM books
+GROUP BY author_lname,
+    author_fname;
